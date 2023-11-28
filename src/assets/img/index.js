ee90994c7fec2e3c09dbc73ce9da4713e205df1f
Zacharia <ndunguzachary24@gmail.com>
2023-11-28 22:48:48 +0300

    Designed service component and exported images

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,1 +1,7 @@
-export { Tap_water as hero_photo } from "./Tap_Water.jpg";
+export { default as Bottle } from "./steve-johnson.jpg";
+export { default as Irrigation } from "./irrigation.jpg";
+export { default as Johnny } from "./johnny.jpg";
+export { default as Irrigation2 } from "./gooseberry-2345496_1280.jpg";
+export { default as Water } from "./fountain-3412242.jpg";
+export { default as Water2 } from "./faucet-1684902_1280.jpg";
+export { default as Water1 } from "./rose-galloway.jpg";
